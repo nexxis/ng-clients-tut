db51de09a83cb738fc54524e1c9dfa7895376709
Evgenii Novikov <ne@sybis.ru>
2015-07-30 15:03:04 +0600

    v-0.3 ub-sybis

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-angular.module('clients', [])
-.config( function($routeProvider) {
-	$routeProvider
-	.when('/', {controller:ListCtrl, templateUrl:'list.html'})
-	// .when('/new', {controller:EditCtrl, templateUrl:'new.html'})
-	.otherwise({redirectTo:'/'});
-});
+angular.module('clients', ['ngRoute'])
+	.config (function ($routeProvider) {
+		$routeProvider
+		.when('/', {
+			controller:'ListClientsCtrl',
+			templateUrl:'list.html'
+		})
+		.when('/new', {
+			controller:'NewClientCtrl',
+			templateUrl:'new.html'
+		})
+		.otherwise({
+			redirectTo:'/'
+		})
+	})
+	.controller('ListClientsCtrl', function( $scope ) {
+		$scope.clients = [{
+			'name': 'Иван',
+			'email': 'ivan@ivan',
+			'phone': '913 xxx xxxx'
+		},
+		{
+			'name': 'Петр',
+			'email': 'petr@petr',
+			'phone': '923 xxx xxxx'
+		}];
+		
+	})
+	.controller('NewClientCtrl', function( $scope ) {
 
+	});
--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,19 @@ gulp.task('html', function () {
   gulp.src('./')
     .pipe(connect.reload());
 });
+gulp.task('js', function () {
+  gulp.src('./')
+    .pipe(connect.reload());
+});
+gulp.task('css', function () {
+  gulp.src('./')
+    .pipe(connect.reload());
+});
 
 gulp.task('watch', function () {
   gulp.watch(['./*.html'], ['html']);
+  gulp.watch(['./*.js'], ['js']);
+  gulp.watch(['./*.css'], ['css']);
 });
 
 gulp.task('default', ['connect', 'watch']);
