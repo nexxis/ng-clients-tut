bc62976480af80b42577ae1f3838a023dee456cd
Evgenii Novikov <ne@sybis.ru>
2015-07-30 18:37:22 +0600

    v-0.4.9 indexDB connected sybis

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-angular.module('clients', ['ngRoute'], ['xc.indexedDB'])
-	.config (function ($routeProvider) {
+angular.module('clients', ['ngRoute', 'indexedDB'])
+	.config (function ($routeProvider, $indexedDBProvider) {
 		$routeProvider
-		.when('/', {
-			controller:'ListClientsCtrl',
-			templateUrl:'list.html'
-		})
-		.when('/new', {
-			controller:'NewClientCtrl',
-			templateUrl:'new.html'
-		})
-		.otherwise({
-			redirectTo:'/'
-		});
-		
+			.when('/', {
+				controller:'ListClientsCtrl',
+				templateUrl:'list.html'
+			})
+			.when('/new', {
+				controller:'NewClientCtrl',
+				templateUrl:'new.html'
+			})
+			.otherwise({
+				redirectTo:'/'
+			});
+
+		$indexedDBProvider
+			.connection('myIndexedDB')
+			.upgradeDatabase(1, function(event, db, tx){
+				var objStore = db.createObjectStore('people', {keyPath: 'ssn'});
+				objStore.createIndex('name_idx', 'name', {unique: false});
+				objStore.createIndex('age_idx', 'age', {unique: false});
+			})
+			.upgradeDatabase(2, function(event, db, tx){
+				db.createObjectStore('peoplePhones', {keyPath: 'person_ssn'});
+			});
 	})
-	.controller('ListClientsCtrl', function( $scope ) {
+	.controller('ListClientsCtrl', function( $scope, $indexedDB ) {
 			$scope.clients = [{
 				'name': 'Иван',
 				'email': 'ivan@ivan',
@@ -24,7 +34,24 @@ angular.module('clients', ['ngRoute'], ['xc.indexedDB'])
 				'name': 'Петр',
 				'email': 'petr@petr',
 				'phone': '923 xxx xxxx'
-			}];		
+			}];
+
+
+			$scope.objects = [];
+
+		    $indexedDB.openStore('people', function(store){
+
+		      store.insert({"ssn": "444-444-222-111","name": "John Doe", "age": 57}).then(function(e){
+		      	console.log("promise");
+		      });
+
+		      store.getAll().then(function(people) {  
+		        // Update scope
+		        $scope.objects = people;
+		      });
+		  })
+
+
 	})
 	.controller('NewClientCtrl', function( $scope ) {
 
