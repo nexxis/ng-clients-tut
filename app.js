1c47ad9d06220c35c0cdf212c31b48f86452ff0e
Evgenii Novikov <ne@sybis.ru>
2015-07-30 12:40:55 +0600

    v-0.2 add gulp&server  ub-sybis

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ angular.module('clients', [])
 .config( function($routeProvider) {
 	$routeProvider
 	.when('/', {controller:ListCtrl, templateUrl:'list.html'})
-	.when('/new', {controller:EditCtrl, templateUrl:'new.html'})
+	// .when('/new', {controller:EditCtrl, templateUrl:'new.html'})
 	.otherwise({redirectTo:'/'});
 });
 
