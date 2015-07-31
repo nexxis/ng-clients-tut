4f3ba343df17549c984b35f3793e335f2261853b
Evgenii Novikov <ne@sybis.ru>
2015-07-31 18:28:43 +0600

    v8

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ angular.module('clients', ['ui.router', 'indexedDB'])
 				controller:'NewClientCtrl',
 				templateUrl:'new.html'
 			})
+			.state('edit', {
+				url: '/edit/:clientId',
+				controller: 'EditClientCtrl',
+				templateUrl:'edit.html'
+			})
 
 		$indexedDBProvider
 			.connection('ClientsDB')
@@ -39,6 +44,32 @@ angular.module('clients', ['ui.router', 'indexedDB'])
 			'name': '',
 			'contacts': []
 		}
+
+		$scope.addContact = function(contactType, contactName) {
+			$scope.person.contacts.push({type: contactType, name: contactName, value: ''})
+		}
+
+		$scope.addClient = function () {
+			$indexedDB.openStore('clients', function(store) {
+				store.insert($scope.person)
+				.then(function() {
+					$state.go('home')
+				});
+			})
+		};
+
+		$scope.delContact = function (contact) {
+			index = $scope.person.contacts.indexOf(contact)
+			if(index > -1) {
+				$scope.person.contacts.splice(index, 1)
+			}
+		};
+
+	})
+	.controller('EditClientCtrl', function( $scope, $indexedDB, $state ) {
+
+		var clientId = $stateParams.clientId;
+		console.log(clientId);
 
 		$scope.addContact = function(contactType, contactName) {
 			$scope.person.contacts.push({type: contactType, name: contactName, value: ''})
