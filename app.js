a5a725b261cd357ca200e4428d7a93548a7108eb
Evgenii Novikov <ne@sybis.ru>
2015-07-31 12:54:00 +0600

    v-6 IDB ok]

--- app.js
+++ app.js
@@ -16,29 +16,39 @@ angular.module('clients', ['ngRoute', 'indexedDB'])
 		$indexedDBProvider
 			.connection('ClientsDB')
 			.upgradeDatabase(1, function(event, db, tx){
-				var objStore = db.createObjectStore('clients', {keyPath: 'id'});
-				objStore.createIndex('name', 'name', {unique: false});
+				var objStore = db.createObjectStore('clients', {keyPath: 'id', autoIncrement:true });
+				objStore.createIndex('name', 'name', {unique: true});
 				objStore.createIndex('email', 'email', {unique: false});
 				objStore.createIndex('phone', 'phone', {unique: false});
-				console.log("upgradeDB OK");
 			})
 	})
 	.controller('ListClientsCtrl', function( $scope, $indexedDB ) {
 
 		    $indexedDB.openStore('clients', function(store){
-
-		      store.insert({'id': 1, 'name': 'Petr Petrovich', 'email': 'petr@petrovich', 'phone': '913-xxx-xx-xx'});
-		      store.insert({'id': 2, 'name': 'Ivan Ivanovich', 'email': 'ivan@ivanovich', 'phone': '923-xxx-xx-xx'});
-		      store.insert({'id': 3, 'name': 'asdfasdf', 'email': 'iasdfh', 'phone': '9asdfxx'});
-
-		      store.getAll().then(function(clients) {  
-		        $scope.clients = clients;
-		        console.log("запись в scope");
-		      });
-		  })
+				// store.insert({'name': 'Petr Petrovich', 'email': 'petr@petrovich', 'phone': '913-xxx-xx-xx'});
+				// store.insert({'name': 'Ivan Ivanovich', 'email': 'ivan@ivanovich', 'phone': '923-xxx-xx-xx'});
+				store.getAll().then(function(clients) {  
+					$scope.clients = clients;
+				});
+			})
 
 
 	})
-	.controller('NewClientCtrl', function( $scope ) {
+	.controller('NewClientCtrl', function( $scope, $indexedDB ) {
+
+		$indexedDB.openStore('clients', function(store){
+			store.getAll().then(function(clients) {  
+				$scope.clients = clients;
+			});
+		})
+
+		$scope.newClient = function () {
+			$indexedDB.openStore('clients', function(store){
+				store.insert({'name': 'New', 'email': 'new@new', 'phone': '9139139131'});
+				store.getAll().then(function(clients) {  
+					$scope.clients = clients;
+				});
+			})
+		};
 
 	});
